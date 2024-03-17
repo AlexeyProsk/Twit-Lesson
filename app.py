d9d31abf276483da759164cbe8a9409db4a073c3
AlexeyProsk <myraskol56@gmail.com>
2024-03-17 19:43:51 +0300

    Added functions: DELETE and UPDATE posts. Class twit.py have new parameter 'id' Updated readme.md

--- app.py
+++ app.py
@@ -6,36 +6,50 @@ twits = []
 app = Flask(__name__)
 
 
-class CustomJSONEnoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Twit):
-            return {'body': obj.body, 'author': obj.author}
-        else:
-            return super().default(obj)
-
-
-app.json_encoder = CustomJSONEnoder
-
-
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({'response': 'pong'})
 
 
+next_id = 1
 @app.route('/twit', methods=['POST'])
 def create_twit():
     """{"body": "My twit", "author": "Username"}
     """
+    global next_id
     twit_json = request.get_json()
-    twit = Twit(twit_json['body'], twit_json['author'])
+    twit = Twit(twit_json['body'], twit_json['author'], id=next_id)
     twits.append(twit)
+    next_id += 1
     return jsonify({'status': 'success'})
 
 
 @app.route('/twit', methods=['GET'])
 def read_twits():
-    serialized_twits = [app.json_encoder.default(None, twit) for twit in twits]
+    serialized_twits = [twit.to_dict() for twit in twits]
     return jsonify(serialized_twits)
+
+
+@app.route('/twit/<int:twit_id>', methods=['PUT'])
+def update_twit(twit_id):
+    """{"body": "UPDATED", "author": "Username"}
+    """
+    twit_json = request.get_json()
+    for twit in twits:
+        if twit.id == twit_id:
+            twit.body = twit_json.get('body', twit.body)
+            twit.author = twit_json.get('author', twit.author)
+            return jsonify({'status': 'success'})
+    return jsonify({'error': 'Twit not found'}), 404
+
+
+@app.route('/twit/<int:twit_id>', methods=['DELETE'])
+def delete_twit(twit_id):
+    for twit in twits:
+        if twit.id == twit_id:
+            twits.remove(twit)
+            return jsonify({'status': 'success'})
+    return jsonify({'error': 'Twit not found'}), 404
 
 
 if __name__ == "__main__":
--- twit.py
+++ twit.py
@@ -3,6 +3,11 @@ from user import User
 
 class Twit:
 
-    def __init__(self, body: str, author: User):
+    def __init__(self, body: str, author: User, id=None):
         self.body = body
         self.author = author
+        self.id = id
+
+
+    def to_dict(self):
+        return {'id': self.id, 'body': self.body, 'author': self.author}
